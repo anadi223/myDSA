d545d94fbec85e6f38ea91808bac6c3894a2cfc2
anadi223 <anadimishra231@gmail.com>
2022-06-01 08:25:06 +0530

    Added Insertion Sort

--- src/com/demo/basicDATASTRUCTURE/Arrays/Sorting/InsertionSort.java
+++ src/com/demo/basicDATASTRUCTURE/Arrays/Sorting/InsertionSort.java
@@ -28,4 +28,22 @@ public class InsertionSort {
         }
     }
 
+    static void insertionSort2(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i-1;
+
+            for(; j>=0;j--){
+
+                if(arr[j] > temp){
+                    
+                    arr[j+1] = arr[j];
+                }else{
+                    break;
+                }
+            }
+            arr[j+1]  = temp;
+        }
+    }
+
 }
