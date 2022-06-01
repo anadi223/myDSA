f38d86e00baeb6fe7a3b8931d1d5fb4ed852a7a8
anadi223 <anadimishra231@gmail.com>
2022-06-01 09:10:51 +0530

    added move zeroes

---
+++ src/com/demo/basicDATASTRUCTURE/Arrays/MoveZeroes.java
@@ -0,0 +1,26 @@
+package com.demo.basicDATASTRUCTURE.Arrays;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        int[] arr = {4,0,5,0,8,0};
+        moveZeroes(arr);
+
+        for(int x: arr){
+            System.out.print(x+" ");
+        }
+    }
+    
+    static void moveZeroes(int[] arr){
+        int i=0;
+
+        for(int j=0;j<arr.length;j++){
+            if(arr[j] != 0){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+
+                i++;
+            }
+        }
+    }
+}
