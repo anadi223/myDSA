0e546fd42d44581ab5a728c69d21b0ec658a4e96
anadi223 <anadimishra231@gmail.com>
2022-06-01 10:18:00 +0530

    Added Check Sorted Rotated Array

---
+++ src/com/demo/basicDATASTRUCTURE/Arrays/CheckIfSortedAndRotate.java
@@ -0,0 +1,34 @@
+package com.demo.basicDATASTRUCTURE.Arrays;
+
+
+public class CheckIfSortedAndRotate{
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+
+        boolean answer = checkSortedRotated(arr);
+
+        System.out.println(answer);
+    }
+
+    static boolean checkSortedRotated(int[] arr){
+
+
+        int count=0;
+
+        for(int i=1;i<arr.length;i++){
+
+            if(arr[i-1] > arr[i]){
+                count++;
+            }
+
+        }
+
+        if(arr[arr.length-1] > arr[0]){
+            count++;
+        }
+
+        return count==1;
+    }
+
+}
