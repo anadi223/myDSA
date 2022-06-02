64d0ddd9a67ab99e9f43e5cd39e988c4492277e7
anadi223 <anadimishra231@gmail.com>
2022-06-02 09:47:09 +0530

    Added Basic Input Output

--- src/com/demo/basicDATASTRUCTURE/Arrays/TwoDArrays/BasicInputOutput.java
+++ src/com/demo/basicDATASTRUCTURE/Arrays/TwoDArrays/BasicInputOutput.java
@@ -1,5 +1,25 @@
 package com.demo.basicDATASTRUCTURE.Arrays.TwoDArrays;
 
+import java.util.Scanner;
+
 public class BasicInputOutput {
-    
+    public static void main(String[] args) {
+        
+
+        int[][] arr = new int[3][3];
+
+        Scanner s1 = new Scanner(System.in);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print("Add new entry");
+                arr[i][j] = s1.nextInt();
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+    }
 }
