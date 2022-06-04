decdef1e239b3f45e21fd4f693324a65d6716e0a
anadi223 <anadimishra231@gmail.com>
2022-06-04 07:57:39 +0530

    Insert a node at head

--- src/com/demo/basicDATASTRUCTURE/LinkedList/LinkedListDemo.java
+++ src/com/demo/basicDATASTRUCTURE/LinkedList/LinkedListDemo.java
@@ -4,21 +4,42 @@ public class LinkedListDemo {
     public static void main(String[] args) {
         
         Node head = new Node(10);
-        Node temp1 = new Node(20);
-        head.next = temp1;
-        temp1.next = null;
 
         System.out.println(head.data);
-        System.out.println(temp1.data);
-        
+
+
+
+        head = insertAtHead(head, 444444);
+        head = insertAtHead(head, 5555);
+
+        printList(head);
+    }
+
+    //printing a Linked List 
+
+    static void printList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.println(curr.data+ " ");
+            curr = curr.next;
+        }
+    }
+
+    //insert data at start
+    static Node insertAtHead(Node head, int x){
+        Node temp = new Node(x);
+        temp.next = head;
+        return temp;
     }
     
 }
-
+ 
 class Node{
 int data;
 Node next;
 
+
+//*constructor */
 public Node(int x){
     data =x;
     next = null;
