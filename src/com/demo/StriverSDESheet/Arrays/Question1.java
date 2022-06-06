73b02ed289b65ad4faed12a12ea399bb95ecefb9
anadi223 <anadimishra231@gmail.com>
2022-06-06 12:01:31 +0530

    Updated better approach method

--- src/com/demo/StriverSDESheet/Arrays/Question1.java
+++ src/com/demo/StriverSDESheet/Arrays/Question1.java
@@ -1,21 +1,35 @@
 package com.demo.StriverSDESheet.Arrays;
 
+import java.util.Arrays;
 
 //https://leetcode.com/problems/set-matrix-zeroes/
 public class Question1 {
      public static void main(String[] args) {
         int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
-        setZeroes(arr);
+        // setZeroes(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[0].length; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+       
+
+        setZeroesBetterApproach(arr);
+        System.out.println("BEtter approch called-----------");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+
+
      }
 
 
-     //!Brute Force Time Complexity :: O(n*m) + O(n+m) Space : O(1)
+     //!Brute Force Time Complexity :: O(n*m) + O(n+m) Space : O(1) Tab hi kaam aayega jab koi bhi element matrix me phle se -1 na ho
      static void setZeroes(int[][] arr){
          int row = arr.length;
          int col = arr[0].length;
@@ -68,6 +82,38 @@ public class Question1 {
          }
 
      }
+
+
+     //* Better Solution With Time complexity O(n*m + n*m) And Space = O(n) + O(m) */
+
+     static void setZeroesBetterApproach(int[][] arr){
+         int rows=arr.length;
+         int cols = arr[0].length;
+         int[] dummy1 = new int[rows];
+         int[] dummy2 = new int[cols];
+         Arrays.fill(dummy1, -1);
+         Arrays.fill(dummy2, -1);
+
+         for(int i=0;i<rows;i++){
+             for(int j=0;j<cols;j++){
+                 if(arr[i][j] ==0){
+                     dummy1[i] =0;
+                     dummy2[j] =0;
+                 }
+             }
+         }
+
+         for(int i=0;i<rows;i++){
+             for(int j=0;j<cols;j++){
+                 if(dummy1[i] ==0 || dummy2[j] ==0){
+                     arr[i][j] = 0;
+                 }
+             }
+         }
+
+     }
+
+
 
 
 }
