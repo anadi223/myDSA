b93305cb60937146616039784cef84db774857cb
anadi223 <anadimishra231@gmail.com>
2022-06-07 07:57:32 +0530

    Doubly LInked List

--- src/com/demo/basicDATASTRUCTURE/LinkedList/DoublyLinkedListDemo.java
+++ src/com/demo/basicDATASTRUCTURE/LinkedList/DoublyLinkedListDemo.java
@@ -1,5 +1,6 @@
 package com.demo.basicDATASTRUCTURE.LinkedList;
 
+import javax.swing.SortOrder;
 
 public class DoublyLinkedListDemo {
     public static void main(String[] args) {
@@ -9,35 +10,21 @@ public class DoublyLinkedListDemo {
 
         
         Node head = new Node(20);
+        Node tail = head;
         printList(head);
         int l = getLinkedListLength(head);
         System.out.println(l);
 
-        // head = insertAtHead(head, 13);
-        // System.out.println("-------------Insert at head called------");
-        // printList(head);
-    DoublyLinkedListDemo dll = new DoublyLinkedListDemo();
-    dll.push(34, head);
-    printList(head);
+        head = insertAtHead(head, 13);
+        System.out.println("-------------Insert at head called------");
+        printList(head);
+
+        tail = insertAtTail(tail, 44);
+        System.out.println("-----------------Insert at tail called---------------");
+        printList(head);
+
     }
 
-    public void push(int new_data,Node head)
-{
-    /* 1. allocate node
-    * 2. put in the data */
-    Node new_Node = new Node(new_data);
- 
-    /* 3. Make next of new node as head and previous as NULL */
-    new_Node.next = head;
-    new_Node.prev = null;
- 
-    /* 4. change prev of head node to new node */
-    if (head != null)
-        head.prev = new_Node;
- 
-    /* 5. move the head to point to the new node */
-    head = new_Node;
-}
 
 
     //printing a list
@@ -67,6 +54,17 @@ public class DoublyLinkedListDemo {
         head.prev = temp;
         return temp;
     }
+    
+    static Node insertAtTail(Node tail, int data){
+        Node temp = new Node(data);
+        
+        tail.next = temp;
+        temp.prev = tail;
+        return temp;
+    }
+
+
+    
 
 }
 
