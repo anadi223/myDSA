7d3bfdc4dcd4b08b88dcf6f72c896524684a9f85
anadi223 <anadimishra231@gmail.com>
2022-06-10 07:55:09 +0530

    Added question 2

---
+++ src/com/demo/StriverSDESheet/Arrays/Question2.java
@@ -0,0 +1,7 @@
+package com.demo.StriverSDESheet.Arrays;
+
+
+//https://www.codingninjas.com/codestudio/problems/1089580?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+public class Question2 {
+    
+}
