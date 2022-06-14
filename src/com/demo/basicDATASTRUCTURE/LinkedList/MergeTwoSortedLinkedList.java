7f2aba004e9d1184b28e008d68e9cca4a37de942
anadi223 <anadimishra231@gmail.com>
2022-06-14 11:31:17 +0530

    Merge two sorted list

--- src/com/demo/basicDATASTRUCTURE/LinkedList/MergeTwoSortedLinkedList.java
+++ src/com/demo/basicDATASTRUCTURE/LinkedList/MergeTwoSortedLinkedList.java
@@ -28,27 +28,27 @@ public class MergeTwoSortedLinkedList {
             temp =temp.next;
         }
     }
-
+        // O(m+n) time
     static Node mergeTwoSorted(Node h1 , Node h2){
         if(h1 == null)
-        return h2;
+        return h2; //if h1 is null then no need to merge
 
         if(h2==null)
-        return h1;
+        return h1; // if h2 is null no need to merge
 
-        Node head  =null;
+        Node head  =null; //head and tail pointer 
         Node tail = null;
 
-        if(h1.data<=h2.data){
+        if(h1.data<=h2.data){ // head aur tail pointer ko us h1 ya h2 ko assign kro jo ki chota hai 
             head =tail = h1;
-            h1 = h1.next;
+            h1 = h1.next; // assign krne ke baad h1 ko aage bdha do
         }else{
             head = tail = h2;
             h2 = h2.next;
         }
 
-        while(h1!=null && h2!=null){
-            if(h1.data<=h2.data){
+        while(h1!=null && h2!=null){ 
+            if(h1.data<=h2.data){ //check karo h1 aur h2 ka data since tail pointer already kisi ek head ko point kr rha toh next pointer kaun sa hoga uske liye aur jo bhi chota hai usko assign kr do tail ke next ko aur tail ko aage bdha do aur head ko aage bdha do
                 tail.next = h1;
                 tail =h1;
                 h1 = h1.next;
@@ -59,7 +59,7 @@ public class MergeTwoSortedLinkedList {
                 h2= h2.next;
             }
         }
-        if(h1 == null){
+        if(h1 == null){ // suppose koi ek list khtm hogyi toh tail ke agle ke baaki bchi hui list pe point kr do isse
             tail.next = h2;
         }else{
             tail.next = h1;
