b3253dd0f57344f6b36f26c85cae89b7309fcefc
anadi223 <anadimishra231@gmail.com>
2022-06-16 18:43:22 +0530

    Two Stack problem

---
+++ src/com/demo/basicDATASTRUCTURE/Stacks/TwoStackProblem.java
@@ -0,0 +1,10 @@
+package com.demo.basicDATASTRUCTURE.Stacks;
+
+//https://www.codingninjas.com/codestudio/problems/two-stacks_983634
+public class TwoStackProblem {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
