09287692bfeacf9d89bcd5911f7509d3edd1a89f
anadi223 <anadimishra231@gmail.com>
2022-06-16 18:43:11 +0530

    Dutch National flag approach as well

--- src/com/demo/StriverSDESheet/Arrays/Question5.java
+++ src/com/demo/StriverSDESheet/Arrays/Question5.java
@@ -4,8 +4,8 @@ package com.demo.StriverSDESheet.Arrays;
 public class Question5 {
     public static void main(String[] args) {
       int[] arr = {1,2,0,0,2,1,1};
-      int[] ans = sort012Appraoch2(arr);
-      
+      //int[] ans = sort012Appraoch2(arr);
+      int[] ans = sort012Approach3(arr);
       for(int x: ans){
         System.out.print(x+ " ");
       }
@@ -15,11 +15,11 @@ public class Question5 {
     //!Appraoch 1 sort kar do he he O(nlogn)
 
 
-    //* Appraoch two count le lo 0 1 2 ka  TC O(N) SC O(N)*/
+    //* Appraoch two count le lo 0 1 2 ka  TC O(2N) SC O(1)*/
 
     static int[] sort012Appraoch2(int[] arr){
         int zeroCount =0;
-        int oneCount =0;
+        int oneCount =0; 
         int twoCount =0;
 
         for(int i=0;i<arr.length;i++){
@@ -33,26 +33,48 @@ public class Question5 {
             }
         }
 
-        int[] ans = new int[arr.length];
+
         int index=0;
         for(int i=0;i<arr.length;i++){
             if(zeroCount!=0){
-                ans[index++] = 0;
+                arr[index++] = 0;
             zeroCount--;
             }
             else if(oneCount!=0){
-                ans[index++] =1;
+                arr[index++] =1;
                 oneCount--;
             }
             else if(twoCount!=0){
-                ans[index++] = 2;
+                arr[index++] = 2;
             }
         }
-        return ans;
+        return arr;
     }
 
-    // sort without using extra space i.e. in place
-    static void sort012Approach3(int[] arr){
+    // sort in one traversal with TC O(N) and SC O(1) //? Variation of dutch national flag algorithm
+    static int[] sort012Approach3(int[] arr){
+        int low =0, mid =0; // Take three pointers low mid and high 
+        int high = arr.length-1;
 
+        for(int i=0;i<arr.length;i++){
+            //MID TRAVERSE krwao 
+            if(arr[mid] ==0){  //Agar mid tmhra 0 dekha toh low poisiton ke array ko swap krwa do mid position wale se aur low++ and mid++
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1){ //agar mid tmhra 1 hai toh sirf mid++
+                mid++;
+            }
+            else if(arr[mid] ==2){ //agar mid tmhra 2 hai toh swap krwao high aur mid ko and high-- only
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+        return arr;
     }
 }
