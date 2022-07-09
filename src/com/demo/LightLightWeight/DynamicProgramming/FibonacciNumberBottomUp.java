7bdbc935ab3e91587e55ca7b49e002b2143d49ff
anadi223 <anadimishra231@gmail.com>
2022-07-09 18:13:43 +0530

    Fibonacci Bottom Up and Space Optimized

--- src/com/demo/LightLightWeight/DynamicProgramming/FibonacciNumberBottomUp.java
+++ src/com/demo/LightLightWeight/DynamicProgramming/FibonacciNumberBottomUp.java
@@ -21,4 +21,20 @@ public class FibonacciNumberBottomUp {
         }
         return dp[n];
         }
+
+
+        //Space Optimized Approach
+        //TC O(n) //! SC O (1)
+        static int findFibAppraochBetter(int n){
+            int prev1 = 1;
+            int prev2 = 0;
+
+            for(int i=2;i<=n;i++){
+                int curr = prev1+prev2; // ek answer nikl gya toh agli call pe 2 aage jayega 3 pe aur 3 ke liye uske peeche wale elements bhi toh ek ek aage jayenge na 
+
+                prev2 = prev1; // prev2 ko update kr diya
+                prev1 = curr; //prev1 ko curr ke barabar kr diya
+            }
+            return prev1; // return kr diya prev1
+        }
 }
